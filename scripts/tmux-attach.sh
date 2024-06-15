4453898b2a2a69bbc92ae3c56f2e0170527d9287
Derrek Landauer <Derrekito@users.noreply.github.com>
2024-06-14 20:02:13 -0600

    fixed tmux script

--- scripts/tmux-attach.sh
+++ scripts/tmux-attach.sh
@@ -1,14 +1,18 @@
-##!/bin/bash
+#!/bin/bash
 
-# Attempt to attach to the first unoccupied session if it exists
-unoccupied_session=$(tmux list-sessions -F "#{session_name}:#{session_attached}" | awk -F: '$2==0 {print $1; exit}')
-
-if [ -n "$unoccupied_session" ]; then
-    # If there is an unoccupied session, attach to it
-    exec tmux attach-session -t "$unoccupied_session"
+# Check if tmux server is running
+if ! tmux has-session 2>/dev/null; then
+    # No tmux server running, start a new session
+    exec tmux new-session -s default
 else
-    # If no unoccupied session exists, create a new one
-    exec tmux new-session
-fi
+    # Attempt to attach to the first unoccupied session if it exists
+    unoccupied_session=$(tmux list-sessions -F "#{session_name}:#{session_attached}" | awk -F: '$2==0 {print $1; exit}')
 
-sleep 1
+    if [ -n "$unoccupied_session" ]; then
+        # If there is an unoccupied session, attach to it
+        exec tmux attach-session -t "$unoccupied_session"
+    else
+        # If no unoccupied session exists, create a new one
+        exec tmux new-session -A -s default
+    fi
+fi
